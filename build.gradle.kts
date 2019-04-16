4f0a15de450e5360b9a4f2bf73de872e17e6f73c
Danilo Pianini <danilo.pianini@unibo.it>
2019-04-16 10:20:21 +0200

    Use the publish-on-central plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,13 @@
 import java.net.URI
 
 plugins {
+    application
     kotlin("jvm") version "1.3.21"
+    `maven-publish`
+    signing
     id("com.palantir.git-version") version "0.12.0-rc2"
     id ("org.danilopianini.git-sensitive-semantic-versioning") version "0.1.0"
-    application
+    id ("org.danilopianini.publish-on-central") version "0.1.1"
 }
 
 repositories {
@@ -23,4 +26,31 @@ dependencies {
 
 application {
     mainClassName = "it.unibo.protelis2kotlin.AppKt"
+}
+
+publishOnCentral {
+    projectDescription.set("A translator from documented Protelis code to compiling Kotlin interfaces")
+    projectLongName.set("Protelis KDoc generator")
+    projectUrl.set("https://github.com/Protelis/Protelis-KDoc-generator")
+    scmConnection.set("git@github.com:Protelis/Protelis-KDoc-generator.git")
+}
+
+publishing {
+    publications {
+        withType<MavenPublication> {
+            pom {
+                developers {
+                    developer {
+                        name.set("Danilo Pianini")
+                        email.set("danilo.pianini@unibo.it")
+                        url.set("http://www.danilopianini.org/")
+                    }
+                    developer {
+                        name.set("Roberto Casadei")
+                        email.set("roby.casadei@unibo.it")
+                    }
+                }
+            }
+        }
+    }
 }
