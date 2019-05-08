31c8db98cf8f6ea0115239723e6bdb64da068609
Roberto Casadei <robyonrails@gmail.com>
2019-05-09 00:50:43 +0200

    Remove unused import insensately preventing compilation

--- src/main/kotlin/it/unibo/protelis2kotlin/Protelis2KotlinPlugin.kt
+++ src/main/kotlin/it/unibo/protelis2kotlin/Protelis2KotlinPlugin.kt
@@ -1,9 +1,8 @@
 package it.unibo.protelis2kotlin
 
 import org.gradle.api.Plugin
-import org.gradle.api.provider.Property
 import org.gradle.api.Project
-import java.io.File
+import org.gradle.api.provider.Property
 
 inline fun <reified T> Project.propertyWithDefault(default: T): Property<T> =
         objects.property(T::class.java).apply { convention(default) }
