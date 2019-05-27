4efb21e52c86af9275a1aa98d7ca6f0702c0f8d1
Roberto Casadei <robyonrails@gmail.com>
2019-05-27 12:25:05 +0200

    Some hygiene (checking double imports) + refactoring

--- src/main/kotlin/it/unibo/protelis2kotlin/Protelis2KotlinDocPlugin.kt
+++ src/main/kotlin/it/unibo/protelis2kotlin/Protelis2KotlinDocPlugin.kt
@@ -20,7 +20,8 @@ open class Protelis2KotlinDocPluginExtension @JvmOverloads constructor(
     val destDir: Property<String> = project.propertyWithDefault(project.buildDir.path + "/protelis-docs/"),
     val kotlinVersion: Property<String> = project.propertyWithDefault("+"),
     val protelisVersion: Property<String> = project.propertyWithDefault("+"),
-    val outputFormat: Property<String> = project.propertyWithDefault("javadoc")
+    val outputFormat: Property<String> = project.propertyWithDefault("javadoc"),
+    val automaticDependencies: Property<Boolean> = project.propertyWithDefault(true)
 )
 
 /**
@@ -35,35 +36,64 @@ class Protelis2KotlinDocPlugin : Plugin<Project> {
     private val dokkaTaskName = "dokka"
     private val dokkaPluginName = "org.jetbrains.dokka"
     private val kotlinPluginName = "org.jetbrains.kotlin.jvm"
+    private val protelis2KotlinPlugin = "Protelis2Kotlin"
+    private val protelis2KotlinDocPlugin = "Protelis2KotlinDoc"
+
+    private val protelisGroup = "org.protelis"
+    private val protelisInterpreterDepName = "protelis-interpreter"
+    private val kotlinGroup = "org.jetbrains.kotlin"
+    private val kotlinStdlibDepName = "kotlin-stdlib"
+
+    private val implConfiguration = "implementation"
 
     override fun apply(project: Project) {
-        val extension = project.extensions.create("Protelis2KotlinDoc", Protelis2KotlinDocPluginExtension::class.java, project)
+        val extension = project.extensions.create(protelis2KotlinDocPlugin, Protelis2KotlinDocPluginExtension::class.java, project)
         if (JavaVersion.current() > JavaVersion.VERSION_1_8) extension.outputFormat.set("html")
 
-        project.repositories.add(project.repositories.jcenter())
-        project.repositories.add(project.repositories.mavenCentral())
 
-        project.pluginManager.apply(kotlinPluginName)
+        if(!project.repositories.contains(project.repositories.jcenter())) {
+            project.repositories.add(project.repositories.jcenter())
+        }
+        if(!project.repositories.contains(project.repositories.mavenCentral())) {
+            project.repositories.add(project.repositories.mavenCentral())
+        }
+
+        if(!project.pluginManager.hasPlugin(kotlinPluginName)) {
+            project.pluginManager.apply(kotlinPluginName)
+        }
 
         // Add dependency to Kotlin stdlib for TODO()s and Protelis
-        project.dependencies.add("implementation", "org.jetbrains.kotlin:kotlin-stdlib:${extension.kotlinVersion.get()}")
-        project.dependencies.add("implementation", "org.protelis:protelis-interpreter:${extension.protelisVersion.get()}")
+        if(extension.automaticDependencies.get()) {
+            val deps = project.configurations.getByName(implConfiguration).dependencies
+            if(!deps.any { it.group==kotlinGroup && it.name==kotlinStdlibDepName }) {
+                project.dependencies.add(implConfiguration, "$kotlinGroup:$kotlinStdlibDepName:${extension.kotlinVersion.get()}")
+            }
 
-        project.pluginManager.apply(Protelis2KotlinPlugin::class.java)
-        project.pluginManager.apply(dokkaPluginName)
+            if(!deps.any { it.group==protelisGroup && it.name==protelisInterpreterDepName }) {
+                project.dependencies.add(implConfiguration, "$protelisGroup:$protelisInterpreterDepName:${extension.protelisVersion.get()}")
+            }
+        }
 
-        // project.plugins.forEach { println("Plugin: $it") }
-        // val dokka = project.plugins.getAt(dokkaPluginName)
+        if(!project.pluginManager.hasPlugin(protelis2KotlinDocPlugin)) {
+            project.pluginManager.apply(Protelis2KotlinPlugin::class.java)
+        }
+        if(!project.pluginManager.hasPlugin(dokkaPluginName)) {
+            project.pluginManager.apply(dokkaPluginName)
+        }
+
+        // Configure Protelis2Kotlin plugin
         val p2kp = project.extensions.getByName("Protelis2Kotlin") as Protelis2KotlinPluginExtension
         p2kp.destDir.set(project.buildDir.path + "/protelis2kotlin/src/main/kotlin")
         val protelis2kotlintask = project.tasks.getByName(generateKotlinFromProtelisTaskName)
         protelis2kotlintask.dependsOn(configureGenerateProtelisDocTaskName)
 
+        // Configure Dokka plugin
         val dokkaTask = project.tasks.getByName(dokkaTaskName)
         dokkaTask.setProperty("jdkVersion", 8)
         dokkaTask.setProperty("reportUndocumented", true)
         dokkaTask.dependsOn(compileKotlinTaskName)
 
+        // Configure Kotlin plugin
         val kotlinPluginExt = project.extensions.getByName("kotlin") as KotlinJvmProjectExtension
         val mainKotlinSrcset = kotlinPluginExt.sourceSets.getByName("main")
         // This doesn't work: mainKotlinSrcset.kotlin.srcDirs.add(File(p2kp.destDir.get()))
