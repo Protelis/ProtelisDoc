970e46738c87e699467527c478835b06922e55c4
Danilo Pianini <danilo.pianini@gmail.com>
2022-03-05 13:13:45 +0100

    build: drop refreshVersions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,28 +1,12 @@
-import org.danilopianini.VersionAliases.justAdditionalAliases
-
 plugins {
-    id("de.fayard.refreshVersions") version "0.10.1"
     id("com.gradle.enterprise") version "3.6.3"
-}
-
-refreshVersions {
-    extraArtifactVersionKeyRules = justAdditionalAliases
-}
-
-buildscript {
-    repositories {
-        gradlePluginPortal()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("org.danilopianini:refreshversions-aliases:+")
-    }
 }
 
 gradleEnterprise {
     buildScan {
         termsOfServiceUrl = "https://gradle.com/terms-of-service"
         termsOfServiceAgree = "yes"
+        publishOnFailure()
     }
 }
 
