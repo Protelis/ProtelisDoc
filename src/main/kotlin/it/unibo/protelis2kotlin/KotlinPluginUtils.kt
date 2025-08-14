98b91e7142375fda905f99c5eec41c75b3eb1a74
Danilo Pianini <danilo.pianini@gmail.com>
2025-08-14 13:46:48 +0200

    chore: bind helper types to non-nullables

--- src/main/kotlin/it/unibo/protelis2kotlin/KotlinPluginUtils.kt
+++ src/main/kotlin/it/unibo/protelis2kotlin/KotlinPluginUtils.kt
@@ -4,11 +4,11 @@ import org.gradle.api.Project
 import org.gradle.api.provider.Property
 import org.gradle.api.provider.Provider
 
-internal inline fun <reified T> Project.propertyWithDefault(default: T): Property<T> =
+internal inline fun <reified T : Any> Project.propertyWithDefault(default: T): Property<T> =
     objects.property(T::class.java).apply { convention(default) }
 
-internal inline fun <reified T> Project.propertyWithDefault(noinline default: () -> T): Property<T> =
+internal inline fun <reified T : Any> Project.propertyWithDefault(noinline default: () -> T): Property<T> =
     objects.property(T::class.java).apply { convention(default()) }
 
-internal inline fun <reified T> Project.propertyWithDefault(default: Provider<T>): Property<T> =
+internal inline fun <reified T : Any> Project.propertyWithDefault(default: Provider<T>): Property<T> =
     objects.property(T::class.java).apply { set(default) }
